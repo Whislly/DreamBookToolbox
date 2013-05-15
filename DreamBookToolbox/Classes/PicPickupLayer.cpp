8f4a0b0eb3fe976456d578e47e962d7a9825f75d
ShadowLei <shadow.lei.008@gmail.com>
2013-05-16 00:36:02 +0800

    small modification on PicPickup

--- DreamBookToolbox/Classes/PicPickupLayer.cpp
+++ DreamBookToolbox/Classes/PicPickupLayer.cpp
@@ -2,8 +2,8 @@
 
 PicPickupLayer::PicPickupLayer(void)
 {
-		this->selectionLayer = NULL;
-		this->container = NULL;
+	this->selectionLayer = NULL;
+	this->container = NULL;
 }
 
 PicPickupLayer::~PicPickupLayer(void)
@@ -12,216 +12,202 @@ PicPickupLayer::~PicPickupLayer(void)
 
 void PicPickupLayer::update(float dt)
 {
-		//It is recommended that a fixed time step is used with Box2D for stability
-		//of the simulation, however, we are using a variable time step here.
-		//You need to make an informed choice, the following URL is useful
-		//http://gafferongames.com/game-physics/fix-your-timestep/
+	//It is recommended that a fixed time step is used with Box2D for stability
+	//of the simulation, however, we are using a variable time step here.
+	//You need to make an informed choice, the following URL is useful
+	//http://gafferongames.com/game-physics/fix-your-timestep/
 
-		int velocityIterations = 8;
-		int positionIterations = 1;
+	int velocityIterations = 8;
+	int positionIterations = 1;
 
-		// Instruct the world to perform a single step of simulation. It is
-		// generally best to keep the time step and iterations fixed.
-		//world->Step(dt, velocityIterations, positionIterations);
-}
-
-void PicPickupLayer::load()
-{
-    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-
-    //TODO here: move to somewhere later.
-    PicShowLayer *current = this->container->GetCurrentShowLayer();
-    current->setPosition(30, 30);
-
-    PicShowLayer *next = this->container->GoToNext();
-    if (next)
-    {
-        //move current
-        CCMoveTo *moveCur = CCMoveTo::create(5, ccp(-winSize.width - 30, 30));
-        current->runAction(moveCur);
-
-        //CCCamera *camCur = current->getCamera();
-        //camCur->setEyeXYZ(0, 0, 415/2);
-        //camCur->setCenterXYZ(0, 0, 0);
-        CCOrbitCamera *orbitCur = CCOrbitCamera::create(5, 1, 0, 0, -160, 0, 0);
-        current->runAction(orbitCur);
-
-        //go to next
-        next->setPosition(winSize.width + 30, 30);
-        next->setVisible(true);
-        CCMoveTo *moveNext = CCMoveTo::create(3, ccp(30, 30));
-        next->runAction(moveNext);
-
-        CCCamera *camNext = current->getCamera();
-        CCOrbitCamera *orbitNext = CCOrbitCamera::create(3, -1, 0, 270, -90, 0, 0);
-
-        next->runAction(orbitNext);
-    }
+	// Instruct the world to perform a single step of simulation. It is
+	// generally best to keep the time step and iterations fixed.
+	//world->Step(dt, velocityIterations, positionIterations);
 }
 
 bool PicPickupLayer::init()
 {
-		CCSize winSize = CCDirector::sharedDirector()->getWinSize();
-		//CCPoint leftTop = VisibleRect::leftTop();
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	//CCPoint leftTop = VisibleRect::leftTop();
 
-		//
-		cocos2d::CCLabelTTF *label = CCTextFieldTTF::textFieldWithPlaceHolder("Search :", CCSize(0, 0), kCCTextAlignmentLeft, "Thonburi", 32);
-		label->setColor(ccc3(121, 112, 1));
-		label->setPosition(ccp(100, winSize.height - 100));
-		label->setAnchorPoint(CCPointZero);
-		this->addChild(label);
+	//
+	cocos2d::CCLabelTTF *label = CCTextFieldTTF::textFieldWithPlaceHolder("Search :", CCSize(0, 0), kCCTextAlignmentLeft, "Thonburi", 32);
+	label->setColor(ccc3(121, 112, 1));
+	label->setPosition(ccp(100, winSize.height - 100));
+	label->setAnchorPoint(CCPointZero);
+	this->addChild(label);
 
-		//
-		CCSprite *search = CCSprite::create("Images/PicPickup/search.png");
-		search->setPosition(ccp(250, winSize.height - 120));
-		search->setAnchorPoint(CCPointZero);
-		this->addChild(search);
+	//
+	CCSprite *search = CCSprite::create("Images/PicPickup/search.png");
+	search->setPosition(ccp(250, winSize.height - 120));
+	search->setAnchorPoint(CCPointZero);
+	this->addChild(search);
 
-		//
-		this->container = DynamicLayerContainer::create(20);
-		this->container->setAnchorPoint(CCPointZero);
-		this->container->setPosition(0, 0);
-		this->addChild(this->container);
-		
-		//
-		this->selectionLayer = PicSelectionLayer::create();
-		selectionLayer->setAnchorPoint(CCPointZero);
-		selectionLayer->setPosition(winSize.width - 240 - 10, 30);
-		this->addChild(selectionLayer);
+	//
+	this->container = DynamicLayerContainer::create(20);
+	this->container->setAnchorPoint(CCPointZero);
+	this->container->setPosition(0, 0);
+	this->addChild(this->container);
 
-        CCMenuItemSprite *pBackItem = CCMenuItemSprite::create(
-            CCSprite::createWithSpriteFrameName("b1.png"), CCSprite::createWithSpriteFrameName("b2.png"), this,
-            menu_selector(PicPickupLayer::menuBackCallback));
+	//
+	this->selectionLayer = PicSelectionLayer::create();
+	selectionLayer->setAnchorPoint(CCPointZero);
+	selectionLayer->setPosition(winSize.width - 240 - 10, 30);
+	this->addChild(selectionLayer);
 
-        // Place the menu item bottom-right conner.
-        pBackItem->setPosition(ccp(CCDirector::sharedDirector()->getWinSize().width - 50, 20));
+	CCMenuItemSprite *pBackItem = CCMenuItemSprite::create(
+		CCSprite::createWithSpriteFrameName("b1.png"),
+		CCSprite::createWithSpriteFrameName("b1.png"),
+		this,
+		menu_selector(PicPickupLayer::menuBackCallback));
+	pBackItem->setPosition(ccp(winSize.width - 50, 20));
 
-        // Create a menu with the "close" menu item, it's an auto release object.
-        CCMenu* pMenu = CCMenu::create(pBackItem, NULL);
-        pMenu->setPosition(CCPointZero);
+	CCMenuItemSprite *preItem = CCMenuItemSprite::create(
+		CCSprite::createWithSpriteFrameName("b2.png"),
+		CCSprite::createWithSpriteFrameName("b2.png"),
+		this,
+		menu_selector(PicPickupLayer::menuPreCallback));
+	preItem->setAnchorPoint(CCPointZero);
+	preItem->setPosition(ccp(30, 10));
 
-        // Add the menu to HelloWorld layer as a child layer.
-        this->addChild(pMenu, 1);
+	CCMenuItemSprite *subItem = CCMenuItemSprite::create(
+		CCSprite::create("b3.png"),
+		CCSprite::create("b3.png"),
+		this,
+		menu_selector(PicPickupLayer::menuSubCallback));
+	int width = winSize.width - 30 - 260 + 30 - subItem->getContentSize().width;
+	subItem->setAnchorPoint(CCPointZero);
+	subItem->setPosition(ccp(width, 10));
 
-        this->load();
+	// Create a menu with the "close" menu item, it's an auto release object.
+	CCMenu* pMenu = CCMenu::create(pBackItem, preItem, subItem, NULL);
+	pMenu->setPosition(CCPointZero);
 
-		return true;
+	// Add the menu to HelloWorld layer as a child layer.
+	this->addChild(pMenu, 1);
+
+
+	//init the show layer
+
+	PicShowLayer *current = this->container->GetCurrentShowLayer();
+	current->setPosition(30, 30);
+
+	return true;
 }
 
 void PicPickupLayer::draw()
 {
-		//
-		// IMPORTANT:
-		// This is only for debug purposes
-		// It is recommend to disable it
-		//
-		CCLayer::draw();
+	//
+	// IMPORTANT:
+	// This is only for debug purposes
+	// It is recommend to disable it
+	//
+	CCLayer::draw();
 
-		ccGLEnableVertexAttribs( kCCVertexAttribFlag_Position );
+	ccGLEnableVertexAttribs( kCCVertexAttribFlag_Position );
 
-		kmGLPushMatrix();
+	kmGLPushMatrix();
 
-		kmGLPopMatrix();
+	kmGLPopMatrix();
 }
 
 void PicPickupLayer::ClickOnSprite(CCPoint location, CCSprite *sprite)
 {
-		//pos
-		CCSize newSize = sprite->getContentSize();
-		newSize = CCSize(newSize.width / 2, newSize.height / 2);
-		CCPoint newPos = this->selectionLayer->GetPreAddPos(newSize);
-		CCPoint selectLayerPos = this->selectionLayer->getPosition();
-		newPos = ccp(newPos.x + selectLayerPos.x, newPos.y + selectLayerPos.y);
+	//pos
+	CCSize newSize = sprite->getContentSize();
+	newSize = CCSize(newSize.width / 2, newSize.height / 2);
+	CCPoint newPos = this->selectionLayer->GetPreAddPos(newSize);
+	CCPoint selectLayerPos = this->selectionLayer->getPosition();
+	newPos = ccp(newPos.x + selectLayerPos.x, newPos.y + selectLayerPos.y);
 
-		//add new sprite to right panel
-		CCSprite *newSprite = CommonHelper::CloneSprite(sprite);
-		newSprite->setPosition(newPos);
-		newSprite->setScale(0.5);
-		newSprite->setContentSize(CCSize(newSprite->getContentSize().width / 2, newSprite->getContentSize().height / 2));
-		newSprite->setVisible(false);
-		this->selectionLayer->AddElement(newSprite);
+	//add new sprite to right panel
+	CCSprite *newSprite = CommonHelper::CloneSprite(sprite);
+	newSprite->setPosition(newPos);
+	newSprite->setScale(0.5);
+	newSprite->setContentSize(CCSize(newSprite->getContentSize().width / 2, newSprite->getContentSize().height / 2));
+	newSprite->setVisible(false);
+	this->selectionLayer->AddElement(newSprite);
 
-		//
-		CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	//
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 
-		//run the sport sprite move action
-		CCSprite *sportSprite = CommonHelper::CloneSprite(sprite);
-		
-		CCPoint p = sprite->getPosition();
-		sportSprite->setAnchorPoint(CCPointZero);
-		sportSprite->setPosition(ccp(p.x + 30, p.y + 30));
-		//TODO here : add this sprite to another layer ???
-		this->addChild(sportSprite);
+	//run the sport sprite move action
+	CCSprite *sportSprite = CommonHelper::CloneSprite(sprite);
 
-		CCFiniteTimeAction *move = CCSequence::create(
-				CCMoveTo::create(2, newPos),
-				CCCallFuncND::create(this, callfuncND_selector(PicPickupLayer::RemoveSportSpriteEvent), (void*)sportSprite), 
-				CCCallFuncND::create(this, callfuncND_selector(PicPickupLayer::TransferSpriteEvent), (void*)newSprite), 
-				NULL);
-		sportSprite->runAction(move);
+	CCPoint p = sprite->getPosition();
+	sportSprite->setAnchorPoint(CCPointZero);
+	sportSprite->setPosition(ccp(p.x + 30, p.y + 30));
+	//TODO here : add this sprite to another layer ???
+	this->addChild(sportSprite);
 
-		CCActionInterval*  actionBy = CCRotateBy::create(2,  360);
-		sportSprite->runAction(actionBy);
-		
-		CCScaleTo *scale = CCScaleTo::create(2, 0.5, 0.5);
-		sportSprite->runAction(scale);
+	CCFiniteTimeAction *move = CCSequence::create(
+		CCMoveTo::create(2, newPos),
+		CCCallFuncND::create(this, callfuncND_selector(PicPickupLayer::RemoveSportSpriteEvent), (void*)sportSprite), 
+		CCCallFuncND::create(this, callfuncND_selector(PicPickupLayer::TransferSpriteEvent), (void*)newSprite), 
+		NULL);
+	sportSprite->runAction(move);
 
-		return;
+	CCActionInterval*  actionBy = CCRotateBy::create(2,  360);
+	sportSprite->runAction(actionBy);
+
+	CCScaleTo *scale = CCScaleTo::create(2, 0.5, 0.5);
+	sportSprite->runAction(scale);
+
+	return;
 }
-
 
 //=============================== Touches ===============================
 //Begin
 
 void PicPickupLayer::ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent)
 {
-		CCSetIterator it;
-		CCTouch* touch;
+	CCSetIterator it;
+	CCTouch* touch;
 
-		for ( it = pTouches->begin(); it != pTouches->end(); it++)
-		{
-				touch = (CCTouch*)(*it);
+	for ( it = pTouches->begin(); it != pTouches->end(); it++)
+	{
+		touch = (CCTouch*)(*it);
 
-				if(!touch)
-						break;
+		if(!touch)
+			break;
 
-				CCPoint location = touch->getLocation();
+		CCPoint location = touch->getLocation();
 
-		}
+	}
 }
 
 void PicPickupLayer::ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent)
 {
-		CCSetIterator it;
-		CCTouch* touch;
+	CCSetIterator it;
+	CCTouch* touch;
 
-		for ( it = pTouches->begin(); it != pTouches->end(); it++)
-		{
-				touch = (CCTouch*)(*it);
+	for ( it = pTouches->begin(); it != pTouches->end(); it++)
+	{
+		touch = (CCTouch*)(*it);
 
-				if(!touch)
-						break;
+		if(!touch)
+			break;
 
-				CCPoint location = touch->getLocation();
-		}
+		CCPoint location = touch->getLocation();
+	}
 }
 
 void PicPickupLayer::ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent)
 {
-		//Add a new body/atlas sprite at the touched location
-		CCSetIterator it;
-		CCTouch* touch;
+	//Add a new body/atlas sprite at the touched location
+	CCSetIterator it;
+	CCTouch* touch;
 
-		for ( it = pTouches->begin(); it != pTouches->end(); it++)
-		{
-				touch = (CCTouch*)(*it);
+	for ( it = pTouches->begin(); it != pTouches->end(); it++)
+	{
+		touch = (CCTouch*)(*it);
 
-				if(!touch)
-						break;
+		if(!touch)
+			break;
 
-				CCPoint location = touch->getLocation();
+		CCPoint location = touch->getLocation();
 
-		}
+	}
 }
 
 void PicPickupLayer::ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent)
@@ -232,62 +218,122 @@ void PicPickupLayer::ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent)
 
 void PicPickupLayer::RemoveSportSpriteEvent(CCNode* sender, void* data)
 {
-		CCSprite *sprite = (CCSprite*)data;
-		this->removeChild(sprite, true);
+	CCSprite *sprite = (CCSprite*)data;
+	this->removeChild(sprite, true);
 }
 
 
 void PicPickupLayer::TransferSpriteEvent(CCNode* sender, void* data)
 {
-		CCSprite *sprite = (CCSprite*)data;
-		sprite->setVisible(true);
+	CCSprite *sprite = (CCSprite*)data;
+	sprite->setVisible(true);
 }
 
 CCArray* PicPickupLayer::getElementArray()
 {
-    CCArray* pElementArray = NULL;
-    if (selectionLayer)
-    {
-        pElementArray = selectionLayer->getElementArray();
-    }
-    return pElementArray;
+	CCArray* pElementArray = NULL;
+	if (selectionLayer)
+	{
+		pElementArray = selectionLayer->getElementArray();
+	}
+	return pElementArray;
 }
 
 void PicPickupLayer::setSelectorWithTarget( CCObject *target, SEL_ImportElementsHandler importElementsSelector )
 {
-    this->m_pListener = target;
-    this->m_importElementsSelector = importElementsSelector;
+	this->m_pListener = target;
+	this->m_importElementsSelector = importElementsSelector;
 }
 
 cocos2d::CCScene* PicPickupLayer::scene(CCObject *target, SEL_ImportElementsHandler importElementsSelector)
 {
-    CCScene * scene = NULL;
-    do 
-    {
-        // 'scene' is an autorelease object
-        scene = CCScene::create();
-        CC_BREAK_IF(! scene);
+	CCScene * scene = NULL;
+	do 
+	{
+		// 'scene' is an autorelease object
+		scene = CCScene::create();
+		CC_BREAK_IF(! scene);
 
-        // 'layer' is an autorelease object
-        PicPickupLayer *layer = PicPickupLayer::create();
-        CC_BREAK_IF(! layer);
+		// 'layer' is an autorelease object
+		PicPickupLayer *layer = PicPickupLayer::create();
+		CC_BREAK_IF(! layer);
 
-        layer->setSelectorWithTarget(target, importElementsSelector);
+		layer->setSelectorWithTarget(target, importElementsSelector);
 
-        // add layer as a child to scene
-        scene->addChild(layer);
-    } while (0);
+		// add layer as a child to scene
+		scene->addChild(layer);
+	} while (0);
 
-    // return the scene
-    return scene;
+	// return the scene
+	return scene;
 }
 
 void PicPickupLayer::menuBackCallback( CCObject* pSender )
 {
-    if (this->m_pListener && this->m_importElementsSelector)
-    {
-        (m_pListener->*m_importElementsSelector)(this, selectionLayer->getElementArray());
-    }
+	if (this->m_pListener && this->m_importElementsSelector)
+	{
+		(m_pListener->*m_importElementsSelector)(this, selectionLayer->getElementArray());
+	}
 
-    CCDirector::sharedDirector()->popScene();
+	CCDirector::sharedDirector()->popScene();
+}
+
+void PicPickupLayer::menuPreCallback( CCObject* pSender )
+{
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	PicShowLayer *current = this->container->GetCurrentShowLayer();
+	PicShowLayer *pre = this->container->GoToPre();
+	if (pre == NULL)
+	{
+		return;
+	}
+
+	//move current
+	current->stopAllActions();
+	CCMoveTo *moveCur = CCMoveTo::create(4, ccp(winSize.width + 30, 30));
+	current->runAction(moveCur);
+	CCOrbitCamera *orbitCur = CCOrbitCamera::create(4, 1, 0, 0, 90, 0, 0);
+	current->runAction(orbitCur);
+
+	//move pre
+	pre->stopAllActions();
+	pre->setVisible(true);
+	pre->setPosition(ccp(-winSize.width - 30, 30));
+	//pre->setPosition(-winSize.width - 30, 30);
+	//
+	CCOrbitCamera *orbitPre = CCOrbitCamera::create(4, 1, 0, -90, 90, 0, 0);
+	pre->runAction(orbitPre);
+	CCMoveTo *movePre = CCMoveTo::create(4, ccp(30, 30));
+	pre->runAction(movePre);
+}
+
+void PicPickupLayer::menuSubCallback( CCObject* pSender )
+{
+	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+	PicShowLayer *current = this->container->GetCurrentShowLayer();
+	PicShowLayer *next = this->container->GoToNext();
+	if (next == NULL)
+	{
+		return;
+	}
+
+	//move current
+	current->setPosition(ccp(30, 30));
+	current->stopAllActions();
+	//
+	CCMoveTo *moveCur = CCMoveTo::create(4, ccp(-winSize.width - 30, 30));
+	current->runAction(moveCur);	
+	CCOrbitCamera *orbitCur = CCOrbitCamera::create(4, 1, 0, 0, -90, 0, 0);
+	current->runAction(orbitCur);
+
+	//move next
+	//next->stopAllActions();
+	next->setPosition(ccp(winSize.width + 30, 30));
+	next->setVisible(true);
+	//
+	CCMoveTo *moveNext = CCMoveTo::create(4, ccp(30, 30));
+	next->runAction(moveNext);
+	//
+	CCOrbitCamera *orbitNext = CCOrbitCamera::create(4, 1, 0, 90, -90, 0, 0);
+	next->runAction(orbitNext);
 }
--- DreamBookToolbox/Classes/PicPickupLayer.h
+++ DreamBookToolbox/Classes/PicPickupLayer.h
@@ -21,39 +21,41 @@ class PicSelectionLayer;
 class PicPickupLayer : public CCLayer, public IInputNotifier
 {
 private:
-		DynamicLayerContainer *container;
-		PicSelectionLayer *selectionLayer;
+	DynamicLayerContainer *container;
+	PicSelectionLayer *selectionLayer;
 
-		void RemoveSportSpriteEvent(CCNode* sender, void* data);
-		void TransferSpriteEvent(CCNode* sender, void* data);
+	void RemoveSportSpriteEvent(CCNode* sender, void* data);
+	void TransferSpriteEvent(CCNode* sender, void* data);
 public:
-		PicPickupLayer(void);
-		~PicPickupLayer(void);
+	PicPickupLayer(void);
+	~PicPickupLayer(void);
 
-		CREATE_FUNC(PicPickupLayer);
+	CREATE_FUNC(PicPickupLayer);
 
-		virtual bool init();
-		virtual void draw();
-		void update(float dt);
-		
-		virtual void ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent);
-		virtual void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
-		virtual void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
-		virtual void ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent);
+	virtual bool init();
+	virtual void draw();
+	void update(float dt);
 
-        virtual void ClickOnSprite(CCPoint location, CCSprite *sprite);
+	virtual void ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent);
+	virtual void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
+	virtual void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
+	virtual void ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent);
 
-        CCArray* getElementArray();
-        void load();
+	virtual void ClickOnSprite(CCPoint location, CCSprite *sprite);
+
+	CCArray* getElementArray();
+
 protected:
-    CCObject*       m_pListener;
-    SEL_ImportElementsHandler m_importElementsSelector;
+	CCObject*       m_pListener;
+	SEL_ImportElementsHandler m_importElementsSelector;
 public:
-    // 设置点击精灵的事件处理器
-    void setSelectorWithTarget(CCObject *target, SEL_ImportElementsHandler importElementsSelector);
-    static cocos2d::CCScene* scene(CCObject *target, SEL_ImportElementsHandler importElementsSelector);
-    // a selector callback
-    void menuBackCallback(CCObject* pSender);
+	// 设置点击精灵的事件处理器
+	void setSelectorWithTarget(CCObject *target, SEL_ImportElementsHandler importElementsSelector);
+	static cocos2d::CCScene* scene(CCObject *target, SEL_ImportElementsHandler importElementsSelector);
+	// a selector callback
+	void menuBackCallback(CCObject* pSender);
+	void menuPreCallback(CCObject* pSender);
+	void menuSubCallback(CCObject* pSender);
 };
 
 #endif
