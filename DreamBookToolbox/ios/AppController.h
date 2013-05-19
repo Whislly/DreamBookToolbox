5785b7a4c0f40e0a720e490118b79528fe14f71c
Xiaohu Deng <dxh-125@163.com>
2013-05-19 09:56:31 +0800

    Add ios project file and source code.
    Add ios project file and source code.

---
+++ DreamBookToolbox/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  DreamBookToolboxAppController.h
+//  DreamBookToolbox
+//
+//  Created by 邓 小虎 on 13-5-12.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
